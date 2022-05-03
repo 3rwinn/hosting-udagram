698fd4543bb724375fd287a06bbfe7274a4d1a26
Erwinn Grebo <erwinngrebo@outlook.com>
2022-05-03 18:34:05 +0000

    edit backend deploy.sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,3 @@
-eb init uda-api-env --region us-east-1 --platform "Node.js"
 eb use uda-api-env --profile default
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB DB_PORT=$DB_PORT POSTGRES_HOST=$POSTGRES_HOST AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE AWS_BUCKET=$AWS_BUCKET JWT_SECRET=$JWT_SECRET --profile default
 eb deploy --profile default
